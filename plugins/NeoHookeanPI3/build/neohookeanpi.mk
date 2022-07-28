475cb188dd12634a581bfeb63874f1454c940148
Ankush Aggarwal <ankush_aggarwal2@yahoo.com>
2022-07-28 21:24:23 +0100

    Adding neo-Hookean plugin for 3.0 version

---
+++ plugins/NeoHookeanPI3/build/neohookeanpi.mk
@@ -0,0 +1,31 @@
+
+SRC = $(wildcard $(NHDIR)src/*.cpp)
+OBJ = $(patsubst $(NHDIR)src/%.cpp, %.o, $(SRC))
+DEP = $(patsubst $(NHDIR)src/%.cpp, %.d, $(SRC))
+
+
+SO = libneohookeanpi_$(PLAT).$(SFX)
+LIB = $(NHDIR)build/lib/$(SO)
+
+FECORE = $(FEBLIB)/libfecore.a
+
+FEBIOMECH = $(FEBLIB)/libfebiomech.a
+
+FEBIOLIBS = $(FEBIOMECH) $(FECORE)
+
+$(LIB): $(OBJ)
+ifeq ($(findstring lnx,$(PLAT)),lnx)
+		$(CC) $(LNKFLG) -shared -Wl,-soname,$(SO) -o $(LIB) $(OBJ) $(FEBIOLIBS)
+else ifeq ($(findstring gcc,$(PLAT)),gcc)
+		$(CC) $(LNKFLG) -shared -Wl,-soname,$(SO) -o $(LIB) $(OBJ) $(FEBIOLIBS)
+else
+		$(CC) -dynamiclib -lomp $(FLG) -o $(LIB) $(OBJ) $(FEBIOLIBS)
+endif
+
+%.o: $(NHDIR)src/%.cpp
+	$(CC) $(INC) $(DEF) $(FLG) -MMD -c -o $@ $<
+
+clean:
+	$(RM) *.o *.d $(LIB)
+
+-include $(DEP)
